d593a15a07b4e09d9db3b8a04f8ba98539468c34
Eden Kang <edenkang5517@gmail.com>
2024-06-15 03:03:01 +0900

    fix : time

--- app/util/check.py
+++ app/util/check.py
@@ -7,6 +7,8 @@ def is_time_in_range(datetime_obj, start_time, end_time):
 
 def is_check_time_in_range(datetime_obj):
     STANDARD_CHECK_TIME = 12 if datetime_obj.weekday() == 5 else 18
-    start_time = datetime.combine(datetime_obj.date(), time(STANDARD_CHECK_TIME, 0))
-    end_time = datetime.combine(datetime_obj.date(), time(23, 59, 59))
+    start_time = datetime.combine(
+        datetime_obj.date(), time(STANDARD_CHECK_TIME, 0)
+    ).time()
+    end_time = datetime.combine(datetime_obj.date(), time(23, 59, 59)).time()
     return is_time_in_range(datetime_obj, start_time, end_time)
