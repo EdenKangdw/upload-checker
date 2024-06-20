0b3e0108a6e2feb50314c0fe1a890288635947fa
Eden Kang <edenkang5517@gmail.com>
2024-06-21 01:54:05 +0900

    fix : stage 서버용 db config

--- app/config.py
+++ app/config.py
@@ -10,16 +10,24 @@ class Config:
     MYSQL_USER = "checker"
     MYSQL_PASSWORD = 123456
 
-    MYSQL_PORT = 32072
-    MYSQL_HOST = "svc.sel5.cloudtype.app"
+    # RELEASE VER.
+    # MYSQL_PORT = 32072
+    # MYSQL_HOST = "svc.sel5.cloudtype.app"
+    # KAKAO_REDIRECT_URI = "https://port-0-upload-checker-server-wr4oe2alqv1116q.sel5.cloudtype.app/oauth/kakao/redirect"
+    # CLIENT_REDIRECT_URL = (
+    #     "https://web-upload-checker-front-wr4oe2alqv1116q.sel5.cloudtype.app/getToken"
+
+    # STAGE VER.
+    MYSQL_PORT = 3306
+    MYSQL_HOST = "checker-db-test"
+    KAKAO_REDIRECT_URI = "https://port-0-upload-checker-wr4oe2alqv1116q.sel5.cloudtype.app/oauth/kakao/redirect"
+    CLIENT_REDIRECT_URL = "https://web-upload-checker-front-test-wr4oe2alqv1116q.sel5.cloudtype.app/getToken"
+
+    # LOCAL VER.
     # MYSQL_PORT = 3306
     # MYSQL_HOST = "mysql"
     # KAKAO_REDIRECT_URI = "http://localhost:8000/oauth/kakao/redirect"
     # CLIENT_REDIRECT_URL = "http://localhost:3000/getToken"
-    KAKAO_REDIRECT_URI = "https://port-0-upload-checker-server-wr4oe2alqv1116q.sel5.cloudtype.app/oauth/kakao/redirect"
-    CLIENT_REDIRECT_URL = (
-        "https://web-upload-checker-front-wr4oe2alqv1116q.sel5.cloudtype.app/getToken"
-    )
 
     def __init__(self):
         self.config = StarletteConfig(".env")
